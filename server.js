75ddcae5a73511d33371e5d1a981ea897c82f5aa
R.J. Steinert <rj@rjsteinert.com>
2017-09-28 07:11:42 -0400

    Echo out we are v2.0.0

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World. This is v1.0.0 \n');
+  res.end('Hello World. This is v2.0.0 \n');
 });
 
 server.listen(port, () => {
